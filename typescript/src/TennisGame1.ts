862c7bf660b66acf35b9b3ceff99ead24e95e12d
Hassan Elhoseny <hassanelhoseny@Hassans-MacBook-Air.local>
2026-02-09 23:38:53 +0200

    separate swith case as a helpr

--- typescript/src/TennisGame1.ts
+++ typescript/src/TennisGame1.ts
@@ -40,22 +40,26 @@ export class TennisGame1 implements TennisGame {
           score += "-";
           tempScore = this.m_score2;
         }
-        switch (tempScore) {
-          case 0:
-            score += "Love";
-            break;
-          case 1:
-            score += "Fifteen";
-            break;
-          case 2:
-            score += "Thirty";
-            break;
-          case 3:
-            score += "Forty";
-            break;
-        }
+          score += this.scoreToText(tempScore);
       }
     }
     return score;
   }
+
+    private scoreToText(score: number): string {
+    switch (score) {
+      case 0:
+        return "Love";
+      case 1:
+        return "Fifteen";
+      case 2:
+        return "Thirty";
+      case 3:
+        return "Forty";
+      default:
+        return "";
+    }
+  }
 }
+
+
